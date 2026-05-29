8c7778e7f8e79e185ff120b41e199ec6345798de
itxadii <diamondgamerz49@gmail.com>
2026-05-29 18:43:30 +0530

    Final changes done

--- components/ui/ContinueButton.tsx
+++ components/ui/ContinueButton.tsx
@@ -1,31 +1,43 @@
-"use client"; // This is mandatory for styled-components in Next.js
+"use client";
 
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 
 interface ContinueButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-    children?: React.ReactNode;
+  children?: React.ReactNode;
 }
 
 const ContinueButton = ({ children = "Apply Now", className = "", ...props }: ContinueButtonProps) => {
-    return (
-        <StyledWrapper>
-            <button {...props} className={`continue-application ${className}`}>
-                <div>
-                    <div className="pencil" />
-                    <div className="folder">
-                        <div className="top">
-                            <svg viewBox="0 0 24 27">
-                                <path d="M1,0 L23,0 C23.5522847,-1.01453063e-16 24,0.44771525 24,1 L24,8.17157288 C24,8.70200585 23.7892863,9.21071368 23.4142136,9.58578644 L20.5857864,12.4142136 C20.2107137,12.7892863 20,13.2979941 20,13.8284271 L20,26 C20,26.5522847 19.5522847,27 19,27 L1,27 C0.44771525,27 6.76353751e-17,26.5522847 0,26 L0,1 C-6.76353751e-17,0.44771525 0.44771525,1.01453063e-16 1,0 Z" />
-                            </svg>
-                        </div>
-                        <div className="paper" />
-                    </div>
-                </div>
-                {children}
-            </button>
-        </StyledWrapper>
-    );
+  // FIX: Track if the component has mounted on the client
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  // Prevent rendering on the server to avoid hydration mismatches with styled-components
+  if (!isMounted) {
+    return null; // You can also return a blank <div> with the same height here to prevent layout shift
+  }
+
+  return (
+    <StyledWrapper>
+      <button {...props} className={`continue-application ${className}`}>
+        <div>
+          <div className="pencil" />
+          <div className="folder">
+            <div className="top">
+              <svg viewBox="0 0 24 27">
+                <path d="M1,0 L23,0 C23.5522847,-1.01453063e-16 24,0.44771525 24,1 L24,8.17157288 C24,8.70200585 23.7892863,9.21071368 23.4142136,9.58578644 L20.5857864,12.4142136 C20.2107137,12.7892863 20,13.2979941 20,13.8284271 L20,26 C20,26.5522847 19.5522847,27 19,27 L1,27 C0.44771525,27 6.76353751e-17,26.5522847 0,26 L0,1 C-6.76353751e-17,0.44771525 0.44771525,1.01453063e-16 1,0 Z" />
+              </svg>
+            </div>
+            <div className="paper" />
+          </div>
+        </div>
+        {children}
+      </button>
+    </StyledWrapper>
+  );
 };
 
 const StyledWrapper = styled.div`
